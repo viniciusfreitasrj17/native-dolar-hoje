7927cb2a780e027db118fb41010d686d1a23a4a7
Vinicius Freitas <viniciusfreitasrj17@gmail.com>
2020-07-03 19:55:35 -0300

    Alter Icon and Splash and Testing Crawler from API Host

--- backend/src/funcions/Crawler.ts
+++ backend/src/funcions/Crawler.ts
@@ -5,22 +5,25 @@ const URL = 'https://dolarhoje.com/'
 
 let craw: number = 0
 
-request(URL, function (err, res, body) {
-  if (err) console.log(`Error: ${err}`)
+setInterval(function () {
+  request(URL, function (err, res, body) {
+    if (err) console.log(`Error: ${err}`)
 
-  const $ = cheerio.load(body)
+    const $ = cheerio.load(body)
 
-  let dolar: any = null
-  $('table[class="conversao"] td').each((i, element) => {
-    if (i === 11) {
-      dolar = element.children[0].data // get data element select
-      const arrayWithComma = dolar.split(' ') // separate R$ to number
-      const arrayWithoutComma = arrayWithComma[1].split(',').join('.') // remove comma for point
-      craw = parseFloat(arrayWithoutComma) // converte string tofloat
-      // craw = 6
-      // console.log(craw) // testing backend
-    }
+    let dolar: any = null
+    $('table[class="conversao"] td').each((i, element) => {
+      if (i === 11) {
+        dolar = element.children[0].data // get data element select
+        const arrayWithComma = dolar.split(' ') // separate R$ to number
+        const arrayWithoutComma = arrayWithComma[1].split(',').join('.') // remove comma for point
+        craw = parseFloat(arrayWithoutComma) // converte string tofloat
+        // craw = 6
+        // console.log(craw) // testing backend
+        console.log(craw)
+      }
+    })
   })
-})
+}, 5000)
 
 export { craw }
